7d577db548457170badd040be49f844ec194fd78
Jay Schwerberg <jay@gomerits.com>
2021-11-16 23:48:43 -0800

    Add address variable

--- server.js
+++ server.js
@@ -2,27 +2,20 @@ import fastify from 'fastify';
 import { access, constants, fstat } from 'fs';
 import { readFile } from 'fs/promises';
 
+const address = '127.0.0.1';
 const port = 3000;
 const responsePath = './data/response.txt'
 
 const server = fastify({logger: true});
 
 server.get('/', (request, reply) => {
-  access(responsePath, constants.W_OK, async (err) => {
-    if (err) {
-      const data = await readFile('./data/response.txt')
-      reply.code(200);
-      reply.send({ hello: data.toString().trim() });
-    } else {
-      reply.code(400);
-      reply.send({ error: "response.txt should not be writable" });
-    }
-  })
+  reply.code(200);
+  reply.send({ hello: 'world' });
 });
 
 const start = async () => {
   try {
-    await server.listen(port)
+    await server.listen(port, address)
   } catch (err) {
     server.log.error(err)
     process.exit(1)
